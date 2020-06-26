c7267db1d62357decf49bbc84b9922e6f03f1c84
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-26 14:56:16 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,8 +28,13 @@ class Song
     song
   end
 
-def self.find_by_name(name)
-  @@all.find{|x| x.name == name}
-end
+  def self.find_by_name(name)
+    @@all.find{|x| x.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name
+    self.create_by_name
+  end
 
 end
