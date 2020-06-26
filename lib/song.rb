868a85afa7ca17251860eff257619bdffcc865e4
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-26 15:12:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,6 +19,7 @@ class Song
   def self.new_by_name(name)
     song = self.new
     song.name = name
+    song.save
     song
   end
 
@@ -26,6 +27,7 @@ class Song
     song = self.new
     song.name = name
     @@all << song
+    song.save
     song
   end
 
