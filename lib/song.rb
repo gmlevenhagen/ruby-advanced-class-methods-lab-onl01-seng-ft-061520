778391d8bdfbc9e6ae18573be07ecbe0f766ea84
Garett Levenhagen <garettlevenhagen@gmail.com>
2020-06-26 15:46:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,11 +56,13 @@ class Song
   end
 
   def self.create_from_filename(name)
-    song = self.new
-    song.name = (name.split("-")[1].chomp(".mp3"))
-    song.artist_name = (name.split("-")[0])
-    @@all << song
+    song = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
     song
+  end
+    
   end
 
 
